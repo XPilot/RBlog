cd7c0e898e1f108cb0e2c5678cd2f00f2e7c0799
Radu Paraschiv <xpilot@gmail.com>
2016-01-11 23:29:24 +0200

    Add dedicated post page

---
+++ app/js/pages/post.page.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class PostPage extends Component {
+
+  render() {
+    return (
+      <p>This is a post page</p>
+    );
+  }
+}
+
+export default PostPage;
