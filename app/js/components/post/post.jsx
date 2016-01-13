6e283387f58f76e39594c030ca59eddd4ec5b7c7
Radu Paraschiv <xpilot@gmail.com>
2016-01-13 11:08:00 +0200

    Add edit / delete controls to post

--- app/js/components/post/post.jsx
+++ app/js/components/post/post.jsx
@@ -1,16 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-import {Link} from 'react-router'
+import { Link } from 'react-router';
 import cx from 'classnames';
 
 import './post.scss';
 
 class Post extends Component {
+  // define our props that the component will use
+  // the purpose of this object is to specify each type of props
   static propTypes = {
     children: PropTypes.node.isRequired,
     className: PropTypes.string,
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     lead: PropTypes.node,
+    onPostEdit: PropTypes.func.isRequired,
+    onPostDelete: PropTypes.func.isRequired,
   }
 
   render() {
@@ -20,6 +24,8 @@ class Post extends Component {
       id,
       lead,
       title,
+      onPostEdit,
+      onPostDelete,
     } = this.props;
 
     const postClasses = {
@@ -33,6 +39,13 @@ class Post extends Component {
         </Link>
         <p className="Post-Lead">{lead}</p>
         <div className="Post-Body" dangerouslySetInnerHTML={{ __html: children }} />
+
+        <hr />
+
+        <div className="Post-Controls">
+          <button className="Post-Control Post-Control--edit Button" onClick={onPostEdit}>Edit post</button>
+          <button className="Post-Control Post-Control--delete Button" onClick={onPostDelete}>Delete post</button>
+        </div>
       </div>
     );
   }
