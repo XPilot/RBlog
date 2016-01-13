51ee4872002a3c1a1254ff093844723f1079b3bc
Radu Paraschiv <xpilot@gmail.com>
2016-01-13 11:12:33 +0200

    Update api routes

--- be/api.js
+++ be/api.js
@@ -16,8 +16,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // define routes here
+
+// get all posts
 Router.get('/', function(req, res) {
     res.json(db.getBlogPosts());
+});
+
+// get specific post by id
+Router.get('/post/:postId', function(req, res) {
+  var postId = req.params.postId;
+  res.json(db.getBlogPost(postId));
+});
+
+// delete specific post by id
+Router.delete('/delete/:postId', function(req, res) {
+  var postId = req.params.postId;
+  res.json(db.deleteBlogPost(postId));
 });
 
 // let the app use the routes
