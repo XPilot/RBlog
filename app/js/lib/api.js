24c1f77f995fdf0f69f572d863802d2a768bda1d
Radu Paraschiv <xpilot@gmail.com>
2016-01-13 11:09:10 +0200

    Add new functions to api

--- app/js/lib/api.js
+++ app/js/lib/api.js
@@ -4,3 +4,16 @@ import { API_URL } from '../constants/constants';
 export function fetchPosts() {
   return fetch(API_URL);
 }
+
+export function fetchPost(postId) {
+  return fetch(`${API_URL}post/${postId}`);
+}
+
+export function editPost(postId) {
+}
+
+export function deletePost(postId) {
+  return fetch(`${API_URL}delete/${postId}`, {
+    method: 'delete',
+  });
+}
