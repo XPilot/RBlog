2825231c49c03877303963526ba750d25387101f
Radu Paraschiv <xpilot@gmail.com>
2016-01-14 09:34:59 +0200

    Add reducer for add action

--- app/js/reducers/posts.reducer.js
+++ app/js/reducers/posts.reducer.js
@@ -10,14 +10,42 @@ export default function postsReducer(state = initialState, action) {
   case POSTS.GET:
     return action.payload;
 
+  case POSTS.ADD:
+    let newState = state.slice();
+
+    // remove any previous started add operations
+    newState = newState.filter((item) => {
+      return item.mode !== 'add';
+    });
+
+    // define this as the cancel trigger
+    if(action.payload && !Object.keys(action.payload).length) {
+      return newState;
+    }
+    
+    newState.push(action.payload || {
+      id: `GenericPostId_${Date.now()}`,
+      mode: 'add',
+      title: '',
+      lead: '',
+      body: '',
+    });
+    // add a nice clean empty post template
+
+    return newState;
+
   case POSTS.EDIT: {
     let newState = state.slice();
+
     newState = newState.map((item) => {
-      if (item.id === action.payload) {
-        item.mode = 'edit';
+      if (item.id === action.payload.id) {
+        item.mode = !item.mode ? 'edit' : null;
+        item = action.payload.postContents ? action.payload.postContents : item;
       }
+
       return item;
     });
+
     return newState;
   }
 
