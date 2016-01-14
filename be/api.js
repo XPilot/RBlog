46d0839cd8cc9310f60c05d000ec4f2f81d8368a
Radu Paraschiv <xpilot@gmail.com>
2016-01-14 09:36:40 +0200

    Update routes

--- be/api.js
+++ be/api.js
@@ -28,6 +28,35 @@ Router.get('/post/:postId', function(req, res) {
   res.json(db.getBlogPost(postId));
 });
 
+Router.post('/post', function(req, res) {
+  var post = req.body;
+
+  // do some validations here to prevent
+  // the loss of title and body (required)
+
+  if (!post.id.length || !post.title.length || !post.body.length) {
+    res.json(null);
+    return void(0);
+  }
+  res.json(db.addBlogPost(post));
+});
+
+
+// edit post, id is from body data
+Router.put('/post', function(req, res) {
+  var post = req.body;
+
+  // do some validations here to prevent
+  // the loss of title and body (required)
+
+  if (!post.id.length || !post.title.length || !post.body.length) {
+    res.json(null);
+    return void(0);
+  }
+
+  res.json(db.updateBlogPost(post));
+});
+
 // delete specific post by id
 Router.delete('/delete/:postId', function(req, res) {
   var postId = req.params.postId;
